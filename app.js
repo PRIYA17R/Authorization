98ac8c68c3059384f2bb0d1f2cfa4102887c3171
shashank929 <rewaskar.priya@gmail.com>
2020-06-12 12:35:45 +0530

    implmented hashing function m5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require ("ejs");
 const mongoose = require ("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+const md5 = require ("md5");
 const app = express();
 
 app.use(express.static ("public"));
@@ -21,14 +22,14 @@ const userSchema = new mongoose.Schema({
 
 //console.log(process.env.API_KEY)
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields : ["password"] });
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields : ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
 app.post("/register", function(req, res){
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     });
 newUser.save(function (err){
     if(!err){
@@ -43,7 +44,7 @@ newUser.save(function (err){
 
 app.post("/login", function(req,res){
     const userName =  req.body.username;
-    const password =  req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : userName }, function (err, founduser){
         if(err)
